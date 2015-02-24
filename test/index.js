d04c675b065fed6482e3469a73b928649060d1df
Ryan Lelek <ryanlelek@gmail.com>
2015-02-24 00:54:51 -0800

    Tests: Adding test example using should

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 
 // Modules
 var supertest = require('supertest');
+var should    = require('should');
 
 // Prepare the Test
 var subject = require('../app/index.js');
@@ -16,6 +17,33 @@ describe('Home', function () {
       .expect(200)
       .expect('Hello World')
       .end(done);
+  });
+
+  it('could also use "should" (alternative example)', function (done) {
+
+    // Using should gives you more control
+    // You may not need it and can stick with
+    // supertest's "expect()" parameter.
+    // It's deceptively simple and powerful.
+    // Use what you prefer!
+
+    request.get('/')
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .end(function (error, res) {
+
+        // No Errors Please
+        should.not.exist(error);
+
+        // Check Response Body
+        // Sending JSON? Use res.body instead
+        res.text.should.equal('Hello World');
+
+        // Call done() manually
+        done();
+
+      });
+
   });
 
 });
